7c4cf0f21b909a076a342d9022f03e4aab0f6671
anushka1124 <anushkasrivastava1124@gmail.com>
2024-12-26 09:29:54 +0530

    spinner added

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,20 +2,25 @@ import React, { useEffect, useState } from 'react';
 import { Pagination, Row } from 'antd';
 import Card from './Card';
 import axios from 'axios';
+import Loading from './Loading';
 
 const ProductCard = ({ api }) => {
   const [products, setProducts] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [error, setError] = useState(null);
   const itemsPerPage = 5;
+  const [isLoading, setIsLoading] = useState(false);
 
   // Fetch products from the given API
   const fetchProducts = async () => {
+    setIsLoading(true);
     try {
       const response = await axios.get(api);
+      setIsLoading(false);
       setProducts(response.data);
     } catch (err) {
       console.error('Error fetching products:', err);
+      setIsLoading(false);
       setError('Failed to fetch products. Please try again later.');
     }
   };
@@ -33,35 +38,43 @@ const ProductCard = ({ api }) => {
 
   return (
     <div>
-      {error ? (
-        <div style={{ textAlign: 'center', color: 'red', margin: '20px 0' }}>
-          {error}
-        </div>
+    {
+      isLoading ? (
+        <Loading/>
       ) : (
-        <Row
-          gutter={{
-            xs: 8,
-            sm: 16,
-            md: 24,
-            lg: 32,
-          }}
-        >
-          {paginatedProducts.map((product) => (
-            <Card key={product.id} product={product} />
-          ))}
-        </Row>
-      )}
+        <>
+          {error ? (
+            <div style={{ textAlign: 'center', color: 'red', margin: '20px 0' }}>
+              {error}
+            </div>
+          ) : (
+            <Row
+              gutter={{
+                xs: 8,
+                sm: 16,
+                md: 24,
+                lg: 32,
+              }}
+            >
+              {paginatedProducts.map((product) => (
+                <Card key={product.id} product={product} />
+              ))}
+            </Row>
+          )}
+          {/* Pagination */}
+          <Pagination
+            current={currentPage}
+            onChange={(page) => setCurrentPage(page)}
+            total={products.length}
+            pageSize={itemsPerPage}
+            showSizeChanger={false}
+            align='center'
+            style={{marginBlock: '20px'}}
+          />
+        </>
+      )
+    }
 
-      {/* Pagination */}
-      <Pagination
-        current={currentPage}
-        onChange={(page) => setCurrentPage(page)}
-        total={products.length}
-        pageSize={itemsPerPage}
-        showSizeChanger={false}
-        align='center'
-        style={{marginBlock: '20px'}}
-      />
     </div>
   );
 };
